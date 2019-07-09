7826adb4ff93d4876c054a735c154389fad39a72
Jack Carter <jcarter2014@gmail.com>
2019-07-08 21:56:06 -0400

    Week 2 commit

--- routes.py
+++ routes.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	prediction = regressor.predict([[1, 0, 0]]).round(1)
+	prediction = str(prediction)
+	return render_template("index.html", prediction = prediction)
 
 if __name__ == "__main__":
+	model = joblib.load('regressor.pkl')
 	app.run(debug=True)

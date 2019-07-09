b131a06dd1ec20e3ba8a3d6259ff573963386549
Jack Carter <jcarter2014@gmail.com>
2019-07-08 22:40:11 -0400

    Week 2 sixth update

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 model = joblib.load('regressor.pkl')
 
 @app.route("/")
 def index():
-	prediction = regressor.predict([[1, 0, 0]]).round(1)
+	prediction = 497632
 	prediction = str(prediction)
 	return render_template("index.html", prediction = prediction)
 
